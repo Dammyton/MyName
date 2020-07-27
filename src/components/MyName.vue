1c742deaeb842e87c15368f5cc90854ad521c989
Dammyton <owolabidamilola98@gmail.com>
2020-07-27 16:38:22 +0100

    added my files

---
+++ src/components/MyName.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>{{ name }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyName',
+    props: {
+        name: String
+    }
+}
+</script>
+
+<style lang="css" scoped>
+    h2{
+        color:rgb(76, 0, 255)
+    }
+</style>
